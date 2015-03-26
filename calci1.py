1d0ca4a390afc4558d627ea530cd30d3a72a2869
krisha9 <krishnnaa.09@gmail.com>
2015-03-26 23:07:43 +0530

    Update calci1.py

--- calci1.py
+++ calci1.py
@@ -1,30 +1,27 @@
-print(" 1 To 'Add' two numbers ")
-print(" 2 To 'Subtract' two numbers ")
-print(" 3 To 'Multiplication' two numbers ")
-print(" 4 For 'Division' ")
+print('''1 To 'Add' two numbers 
+2 To 'Subtract' two numbers 
+3 For 'Multiplication' of  two numbers 
+4 For 'Division''')
 
 ct=1
 while ct == 1:
-	choice=int(input("Enter your choice.: "))
-	if choice>4:
+	ch=int(input("Enter your choice: "))
+	if ch>4:
 		print("invalid choice")
 	else:
-		x=int(input("Enter your first no.: "))
-		y=int(input("Enter the second no.: "))
-		if choice == 1:
-			print("The 'Addition' of both the no.s is : ",x+y)
+		x=int(input("Enter your first no: "))
+		y=int(input("Enter the second no: "))
+		if ch == 1:
+			print("The Addition of both the no.s is : ",x+y)
 			
-
-		elif choice== 2:
-			print("The 'subtraction' of  the no.s is : ",x-y)
+		elif ch== 2:
+			print("The subtraction of  the no.s is : ",x-y)
 			
-
-		elif choice == 3:
-		    print("The multiplication of two no.s is : ",x*y)
+		elif ch == 3:
+		    	print("The multiplication of two no.s is : ",x*y)
 		    
-
-		elif choice == 4:
-			print("'Dividing' 1st no. by 2nd no. : ",x/y)
+		elif ch == 4:
+			print(" Dividing 1st no. by 2nd no. : ",x/y)
 			
 		a=input("do you wish to continue to calculate?type(yes/no)")
 		if a=='yes':
